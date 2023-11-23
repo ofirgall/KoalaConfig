4df1c36575884c25e22c3944b049b190143ed1cf
Ofir Gal <ofir@gal.software>
2023-11-23 23:10:06 +0200

    icon-picker

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -5,4 +5,18 @@ table.insert(M, {
 	enabled = false,
 })
 
+table.insert(M, {
+	'ziontee113/icon-picker.nvim',
+	cmd = {
+		'IconPickerInsert',
+		'IconPickerNormal',
+		'IconPickerYank',
+	},
+	config = function()
+		require('icon-picker').setup({
+			disable_legacy_commands = true,
+		})
+	end,
+})
+
 return M
