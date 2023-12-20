84400e3d1f801f4c0fd4943048c1744fd7d4c202
Ofir Gal <ofir@gal.software>
2023-12-20 20:09:47 +0200

    removed koala opts

--- lua/plugins/koala.lua
+++ lua/plugins/koala.lua
@@ -3,24 +3,6 @@ local M = {}
 -- Custom setup of KoalaVim
 table.insert(M, {
 	'KoalaVim/KoalaVim',
-	opts = {
-		autocmds = {
-			half_screen = {
-				full_screen_width = 273,
-			},
-			absolute_lines = false,
-		},
-		plugins = {
-			open_jira = {
-				jira_url = 'https://volumez.atlassian.net/browse/',
-			},
-		},
-		lsp = {
-			format = {
-				blacklist = { 'clangd', 'ruff_lsp' },
-			},
-		},
-	},
 })
 
 return M
