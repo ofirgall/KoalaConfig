a8f1e970755c5208b663fbb84b5d0fe8b780e50a
Ofir Gal <ofirgal753@gmail.com>
2026-02-13 19:32:41 +0200

    move icon-picker to koala

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -5,18 +5,4 @@ table.insert(M, {
 	enabled = false,
 })
 
-table.insert(M, {
-	'ziontee113/icon-picker.nvim',
-	cmd = {
-		'IconPickerInsert',
-		'IconPickerNormal',
-		'IconPickerYank',
-	},
-	config = function()
-		require('icon-picker').setup({
-			disable_legacy_commands = true,
-		})
-	end,
-})
-
 return M
